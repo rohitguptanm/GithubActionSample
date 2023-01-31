0c716a2bd5b3cb6650fac371d85487bfc2da00fa
Rohit Gupta <rohitguptanm@gmail.com>
2023-01-31 19:20:02 +0530

    Fetch json value

--- .github/workflows/abc.js
+++ .github/workflows/abc.js
@@ -1,4 +1,5 @@
 console.log("Print value")
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_AUTH_KEY);
+//const serviceAccount = JSON.parse(process.env.FIREBASE_AUTH_KEY);
+const serviceAccount = require(GOOGLE_APPLICATION_CREDENTIALS);
 console.log(serviceAccount)
